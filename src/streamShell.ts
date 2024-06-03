7f252afe551d098d4e78636ffee12162d0d14814
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-03 17:30:15 +0330

    ShellCommand type updated to support per-platform command configuration

--- src/streamShell.ts
+++ src/streamShell.ts
@@ -1,19 +1,24 @@
 import { exec as _exec } from 'child_process';
 import { Readable } from 'stream';
+import os from 'os';
 
 // * utils
 import logger from './utils/logger';
+import { parseCommand } from './utils';
 
 // * data
 const handlers = {
   cd: (path: string) => process.chdir(path),
 };
 
+// * types
+type ShellCommand = string | Record<NodeJS.Platform, string>;
+
 type StreamShell = {
-  (...commands: string[]): { stdout?: Readable; stderr?: Readable };
+  (...commands: ShellCommand[]): { stdout?: Readable; stderr?: Readable };
 };
 
-const streamShell: StreamShell = (...commands: string[]) => {
+const streamShell: StreamShell = (...commands: ShellCommand[]) => {
   if (!commands.length) {
     logger.warn('no commands were specified');
     return {};
@@ -23,11 +28,17 @@ const streamShell: StreamShell = (...commands: string[]) => {
   const stderr = new Readable({ read() {} });
   let index = 0;
 
-  const exec = (command: string) => {
+  const exec = (command: ShellCommand) => {
     if (!command) {
       stdout.push(null);
       stderr.push(null);
       return;
+    }
+
+    if (typeof command !== 'string') {
+      const osPlatform = os.platform();
+      if (!(osPlatform in command)) throw new Error(`no command was specified for ${osPlatform}`);
+      command = command[osPlatform];
     }
 
     const cp = _exec(command, err => {
@@ -37,15 +48,9 @@ const streamShell: StreamShell = (...commands: string[]) => {
         return;
       }
 
-      command = command.trim();
-      const firstSeparatorIndex = command.indexOf(' ');
-      const commandParts = [
-        command.slice(0, firstSeparatorIndex),
-        command.slice(firstSeparatorIndex + 1),
-      ];
-
-      const cmd = commandParts[0] as keyof typeof handlers;
-      handlers[cmd]?.(commandParts[1]);
+      const parsedCommand = parseCommand(command as string);
+      const cmd = parsedCommand[0] as keyof typeof handlers;
+      handlers[cmd]?.(parsedCommand[1]);
 
       exec(commands[++index]);
     });
