200c5dda8c80e9592ca595ded16e271431c1b748
sina-byn <sina.sina.bayan@protonmail.com>
2024-06-03 17:24:58 +0330

    parseCommand util added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,14 @@
 import os from 'os';
 
 export const isWindows = () => os.platform() === 'win32';
+
+export const parseCommand = (command: string): [string, string] => {
+  command = command.trim();
+  const firstSeparatorIndex = command.indexOf(' ');
+  const commandParts = [
+    command.slice(0, firstSeparatorIndex),
+    command.slice(firstSeparatorIndex + 1),
+  ];
+
+  return [commandParts[0], commandParts[1]];
+};
